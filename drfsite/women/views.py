1032e1b7a96a4fec84590a37f42ceea818958098
Zaza <96164075+Zaza2215@users.noreply.github.com>
2023-03-06 13:02:51 +0200

    6 ListCreateAPIView & ModelSerializer

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -8,9 +8,10 @@ from .models import *
 from .serializers import *
 
 
-# class WomenAPIView(generics.ListAPIView):
-#     queryset = Women.objects.all()
-#     serializer_class = WomenSerializer
+class WomenAPIList(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
 
 class WomenAPIView(APIView):
     def get(self, request):
