084443259f6c7d9d816e40d2d674a1dbc65251f1
Zaza <96164075+Zaza2215@users.noreply.github.com>
2023-03-08 20:47:51 +0200

    15 Pagination
    * Add custom pagination
    * Connect built-in pagination

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, viewsets
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 
@@ -8,10 +9,17 @@ from .serializers import *
 from .permissions import *
 
 
+class WomenAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = "page_size"
+    max_page_size = 10000
+
+
 class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = WomenAPIListPagination
 
 
 class WomenAPIUpdate(generics.RetrieveUpdateDestroyAPIView):
