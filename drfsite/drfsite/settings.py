49e7619d6a785b77fa802b699133dbd310bec3df
Zaza <96164075+Zaza2215@users.noreply.github.com>
2023-03-08 20:00:49 +0200

    12 auth token using djoser

--- drfsite/drfsite/settings.py
+++ drfsite/drfsite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
+    "djoser",
     "women.apps.WomenConfig",
 ]
 
@@ -131,5 +133,10 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
-    ]
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
 }
